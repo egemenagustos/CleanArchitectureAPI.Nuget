e2db39cdd58050c2637a3621d0216f1bdc276100
Egemen Agustos <info@egemenagustos.com>
2024-03-25 22:00:56 +0300

    exception-logging-implementation

---
+++ Core.Application/Pipelines/Logging/LoggingBehavior.cs
@@ -0,0 +1,35 @@
+﻿using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Serilog;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace Core.Application.Pipelines.Logging
+{
+    public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, ILoggableRequest
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly LoggerServiceBase _logger;
+
+        public LoggingBehavior(IHttpContextAccessor contextAccessor, LoggerServiceBase logger)
+        {
+            _contextAccessor = contextAccessor;
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            List<LogParameter> logParameters =
+                new()
+                { new LogParameter{Type = request.GetType().Name, Value = request, }};
+
+            LogDetail logDetail =
+                new()
+                { MethodName = next.Method.Name, LogParameters = logParameters, User = _contextAccessor.HttpContext.User.Identity.Name ?? "" };
+
+            _logger.Info(JsonSerializer.Serialize(logDetail));
+            return await next();
+        }
+    }
+}
